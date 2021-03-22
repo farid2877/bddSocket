73a18e3496f8f1f1b167daa35f25b3d5fbac14c0
mousqtr <49991720+mousqtr@users.noreply.github.com>
2021-03-22 16:24:54 +0100

    Deploy test

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,8 @@
 // Uncomment for deploy
-// const socket = io('https://fast-castle-70914.herokuapp.com/');
+const socket = io('https://acid-login.herokuapp.com/');
 
 // Comment for deploy
-const socket = io('http://localhost:3000');
+// const socket = io('http://localhost:3000');
 
 
 let buttonValider = document.getElementById('validateButton');
