08a72652e526d0c688452c523f3cb055ef78c9f1
mousqtr <49991720+mousqtr@users.noreply.github.com>
2021-03-22 17:34:18 +0100

    Use the same client

--- server/server.js
+++ server/server.js
@@ -34,7 +34,6 @@ io.on('connection', clientSocket => {
         let text = "INSERT INTO utilisateur(id,nom,prenom) VALUES(7,$1,'Branco') RETURNING *"
         let values = [userName];
         try {
-            await client.connect()
             await client.query(text, values);
             console.log("Insert " + userName)
         }
